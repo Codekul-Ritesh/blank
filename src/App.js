6b631943ff919bba58c4e3444e044f851e346d07
Codekul-Ritesh <ritesh.tagunde2402@gmail.com>
2024-01-09 20:38:51 -0800

    demo

--- src/App.js
+++ src/App.js
@@ -51,8 +51,10 @@ function App() {
               <div className="col-md-5">
                 <div className="titlepage">
                   <h2>About Us</h2>
-                  <p>The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum. </p>
-                  <a className="read_more" href="Javascript:void(0)"> Read More</a>
+                  <h3>Welcome to Hotel Ranjai: A Vision by Shrikant Lohkare</h3>
+                  <p>                                        August 2023 marks the inception of Hotel Ranjai, a chapter in the realm of hospitality that promises an unparalleled experience. From the moment our doors opened, we embarked on a journey to provide a haven where comfort, elegance, and personalized service converge seamlessly.
+ </p>
+                  <a className="read_more" href="About"> Read More</a>
                 </div>
               </div>
               <div className="col-md-7">
